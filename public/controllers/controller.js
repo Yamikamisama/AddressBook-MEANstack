0a0bcc39b4804c927eec3023a4b7042327c61d4d
Michael Angelo <yamikamisama@gmail.com>
2015-02-15 13:03:16 -0500

    mongo db database connected and displaying data on screen

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,9 +1,9 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
-    $http.get('/contactList').success(function(response){
+    $http.get('/contactlist').success(function(response){
     	console.log("I got the data I requested");
-    	$scope.contactList = response;
+    	$scope.contactlist = response;
     })
 
 
--- server.js
+++ server.js
@@ -1,34 +1,18 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
 
 app.use(express.static(__dirname + "/public"));
 
-app.get('/contactList', function(req, res){
+app.get('/contactlist', function(req, res){
 	console.log("I recieved a get request");
 
-	person1 = {
-    	name: 'Tim',
-    	email: 'tim@email.com',
-    	number: '(111) 111-1111'
-    };
-
-    person2 = {
-    	name: 'Emily',
-    	email: 'emily@email.com',
-    	number: '(222) 222-2222'
-    };
-
-    person3 = {
-    	name: 'John',
-    	email: 'john@email.com',
-    	number: '(333) 333-3333'
-    };
-
-    var contactList = [person1, person2, person3];
-    res.json(contactList)
-
-
-})
+    db.contactlist.find(function(err, docs){
+        console.log(docs);
+        res.json(docs);
+    });
+});
 
 app.listen(3000);
 console.log("server running on port 3000")
