1877c4429b86b2aee81dc139918fbf516a02dd4f
Michael Angelo <yamikamisama@gmail.com>
2015-02-15 12:07:20 -0500

    dummy data connected from controller

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,22 +3,24 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
 
     person1 = {
-    	name: 'Tim'
-    	email: 'tim@email.com'
+    	name: 'Tim',
+    	email: 'tim@email.com',
     	number: '(111) 111-1111'
     };
 
     person2 = {
-    	name: 'Emily'
-    	email: 'emily@email.com'
+    	name: 'Emily',
+    	email: 'emily@email.com',
     	number: '(222) 222-2222'
     };
 
     person3 = {
-    	name: 'John'
-    	email: 'john@email.com'
+    	name: 'John',
+    	email: 'john@email.com',
     	number: '(333) 333-3333'
     };
 
-    var contactList = [person1, person2, person3]
+    var contactList = [person1, person2, person3];
+    $scope.contactList = contactList;
+
 }]);
