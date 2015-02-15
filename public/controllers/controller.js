8cc01c1d74d41992ad855655c1fdb7e19663995b
Michael Angelo <yamikamisama@gmail.com>
2015-02-15 12:13:35 -0500

    data now being served from the server

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,29 +1,12 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
+    $http.get('/contactList').success(function(response){
+    	console.log("I got the data I requested");
+    	$scope.contactList = response;
+    })
 
-    $http.get('/contactList')
 
-    person1 = {
-    	name: 'Tim',
-    	email: 'tim@email.com',
-    	number: '(111) 111-1111'
-    };
-
-    person2 = {
-    	name: 'Emily',
-    	email: 'emily@email.com',
-    	number: '(222) 222-2222'
-    };
-
-    person3 = {
-    	name: 'John',
-    	email: 'john@email.com',
-    	number: '(333) 333-3333'
-    };
-
-    var contactList = [person1, person2, person3];
-    $scope.contactList = contactList;
 
 
 
