abb41866ff940939d0c9b2289a5a7062fc46ca0a
Michael Angelo <yamikamisama@gmail.com>
2015-02-16 00:29:46 -0500

    data creation is now updating on the page

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,15 +1,22 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
-    $http.get('/contactlist').success(function(response){
+
+var refresh = function(){
+  	$http.get('/contactlist').success(function(response){
     	console.log("I got the data I requested");
     	$scope.contactlist = response;
-    })
+    	$scope.contact = "";
+    });
+};
+
+refresh();
 
 $scope.addContact = function(){
 	console.log($scope.contact)
 	$http.post('/contactlist', $scope.contact).success(function(response){
 		console.log(response);
+		refresh();
 	})
 }
 
